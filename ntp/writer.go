fb8ebd384340ca63ab08bc55ccc0718f297df662
Paul Chung <me@paulchu.ng>
2024-11-03 17:54:40 -0800

    write csv too

--- ntp/writer.go
+++ ntp/writer.go
@@ -3,6 +3,7 @@ package ntp
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"sync"
 	"time"
 
@@ -14,7 +15,8 @@ import (
 type FileManager struct {
 	currentDate string
 	writer      *pcapgo.Writer
-	outputFile  *os.File
+	outputPcap  *os.File
+	outputCsv   *os.File
 	mutex       sync.Mutex
 }
 
@@ -22,11 +24,12 @@ func getCurrentDate() string {
 	return time.Now().Format("2006-01-02")
 }
 
-func getNewFileName(identifier string) string {
+func getNewFileName(identifier string) (string, string) {
 	hours, minutes, seconds := time.Now().Clock()
-	newFilename := fmt.Sprintf("%s_%s_%02d%02d%02d.pcap", "output/"+identifier, getCurrentDate(), hours, minutes, seconds)
+	newPcap := fmt.Sprintf("%s_%s_%02d%02d%02d.pcap", "output/"+identifier, getCurrentDate(), hours, minutes, seconds)
+	newCsv := fmt.Sprintf("%s_%s_%02d%02d%02d.csv", "output/"+identifier, getCurrentDate(), hours, minutes, seconds)
 
-	return newFilename
+	return newPcap, newCsv
 }
 
 func (fm *FileManager) RotateFileIfNeeded(identifier string) error {
@@ -35,34 +38,50 @@ func (fm *FileManager) RotateFileIfNeeded(identifier string) error {
 
 	current := getCurrentDate()
 	if fm.currentDate != current {
-		if fm.outputFile != nil {
-			fm.outputFile.Close()
+		if fm.outputPcap != nil {
+			fm.outputPcap.Close()
+		}
+		if fm.outputCsv != nil {
+			fm.outputCsv.Close()
 		}
 
-		newFilename := getNewFileName(identifier)
+		newPcap, newCsv := getNewFileName(identifier)
 
 		var err error
-		fm.outputFile, err = os.Create(newFilename)
+		fm.outputPcap, err = os.Create(newPcap)
 		if err != nil {
 			return err
 		}
 
-		fm.writer = pcapgo.NewWriter(fm.outputFile)
+		fm.outputCsv, err = os.Create(newCsv)
+		if err != nil {
+			return err
+		}
+		fm.outputCsv.WriteString("timestamp,ip\n")
+		fm.outputCsv, err = os.OpenFile(newCsv, os.O_APPEND, 0666)
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+
+		fm.writer = pcapgo.NewWriter(fm.outputPcap)
 		if err := fm.writer.WriteFileHeader(65536, layers.LinkTypeIPv4); err != nil {
 			return err
 		}
 
 		fm.currentDate = current
-		fmt.Println("[*] Created new packet file:", newFilename)
+		fmt.Println("[*] Created new packet file:", newPcap)
+		fmt.Println("[*] Created new CSV file:", newCsv)
+
 	}
 	return nil
 }
 
-func (fm *FileManager) LogNTPPacket(packet gopacket.Packet) {
+func (fm *FileManager) LogNTPPacket(packet gopacket.Packet, ipString string) {
 	// BEcause for some reason go prepends MAC addresses
 	data := packet.Data()[14:]
 	captureInfo := gopacket.CaptureInfo{
-		Timestamp:     time.Now(),
+		Timestamp:     packet.Metadata().Timestamp,
 		CaptureLength: len(data),
 		Length:        len(data),
 	}
@@ -70,4 +89,7 @@ func (fm *FileManager) LogNTPPacket(packet gopacket.Packet) {
 	fm.mutex.Lock()
 	defer fm.mutex.Unlock()
 	fm.writer.WritePacket(captureInfo, data)
+
+	timeString := strconv.FormatInt(packet.Metadata().Timestamp.Unix(), 10)
+	fm.outputCsv.WriteString(timeString + "," + ipString + "\n")
 }
